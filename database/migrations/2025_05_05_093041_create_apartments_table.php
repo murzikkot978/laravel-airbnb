df435edbe560375280b6a62fd385d8ad305e4cb7
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-05-05 14:01:56 +0200

    feat: added database
    close #10

---
+++ database/migrations/2025_05_05_093041_create_apartments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('rooms');
+            $table->integer('peoples');
+            $table->integer('price');
+            $table->string('photo');
+            $table->string('country');
+            $table->string('city');
+            $table->string('street');
+            $table->integer('reserved');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
