b3ff3f56d22e6f13344d45858b41ff6057511563
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-05-06 09:24:43 +0200

    feat: added models
    close #9

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = [
+      'user_id',
+      'apartment_id',
+      'start_date',
+      'end_date',
+    ];
+
+    protected $table = 'reservations';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function apartment(): BelongsTo
+    {
+        return $this->belongsTo(Apartment::class, 'apartment_id');
+    }
 }
